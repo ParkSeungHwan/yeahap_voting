2d18fcb7fed7570a5d1d6a923019d447ee8363de
seunghwan <rr5ys5s@gmail.com>
2018-04-02 13:34:53 +0900

    truffle cofig 설정 , migration 수정

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,12 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 
+var Voting = artifacts.require("./Voting.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'], {gas: 6700000});
+};
+
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
